f03d6a4a4d66f6447f2352c504f07474a93ca0ca
Chuck <403chuck@gmail.com>
2024-03-28 00:12:36 +0800

    feat: Add puppeteer and browser functionality, handle URL requests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from 'hono'
+import puppeteer from '@cloudflare/puppeteer'
+import type { BrowserWorker } from '@cloudflare/puppeteer'
 
-const app = new Hono()
+type Bindings = {
+  MYBROWSER: BrowserWorker;
+	BROWSER_KV_URLBOX: KVNamespace;
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', async (c) => {
+  let url = c.req.query('url');
+  let img: Buffer|null
+  if (url) {
+    url = new URL(url).toString()
+    img = await c.env.BROWSER_KV_URLBOX.get(url, { type: 'arrayBuffer' }) as Buffer
+    if (img === null) {
+      const browser = await puppeteer.launch(c.env.MYBROWSER)
+      const page = await browser.newPage()
+      await page.goto(url)
+      img = await page.screenshot() as Buffer
+      await c.env.BROWSER_KV_URLBOX.put(url, img, { expirationTtl: 60 * 60 * 24 })
+      await browser.close()
+    }
+    c.header('Content-Type', 'image/jpeg')
+    c.status(200)
+    return c.body(img)
+  } else {
+    c.status(400)
+    return c.json({ error: 'url is required' })
+  }
 })
 
 export default app
