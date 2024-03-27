43ecffdbee9676cc6b995a7e03759ed444c1400f
Chuck <403chuck@gmail.com>
2024-03-28 00:21:14 +0800

    refactor: Refactor code to improve readability and maintainability

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,28 @@ import puppeteer from '@cloudflare/puppeteer'
 import type { BrowserWorker } from '@cloudflare/puppeteer'
 
 type Bindings = {
-  MYBROWSER: BrowserWorker;
-  BROWSER_KV_URLBOX: KVNamespace;
+  MYBROWSER: BrowserWorker
+  BROWSER_KV_URLBOX: KVNamespace
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', async (c) => {
-  let url = c.req.query('url');
-  let img: Buffer|null
+  let url = c.req.query('url')
+  let img: Buffer | null
   if (url) {
     url = new URL(url).toString()
-    img = await c.env.BROWSER_KV_URLBOX.get(url, { type: 'arrayBuffer' }) as Buffer
+    img = (await c.env.BROWSER_KV_URLBOX.get(url, {
+      type: 'arrayBuffer',
+    })) as Buffer
     if (img === null) {
       const browser = await puppeteer.launch(c.env.MYBROWSER)
       const page = await browser.newPage()
       await page.goto(url)
-      img = await page.screenshot() as Buffer
-      await c.env.BROWSER_KV_URLBOX.put(url, img, { expirationTtl: 60 * 60 * 24 })
+      img = (await page.screenshot()) as Buffer
+      await c.env.BROWSER_KV_URLBOX.put(url, img, {
+        expirationTtl: 60 * 60 * 24,
+      })
       await browser.close()
     }
     c.header('Content-Type', 'image/jpeg')
